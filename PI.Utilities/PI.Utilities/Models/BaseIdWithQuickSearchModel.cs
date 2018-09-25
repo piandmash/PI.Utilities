1990d9354fcd58c31e4ffd809d73ff7cdf4f8aa0
Pete Cleary <pete@piandmash.com>
2018-09-25 14:39:52 +0100

    Added in some additional Base Models

---
+++ PI.Utilities/PI.Utilities/Models/BaseIdWithQuickSearchModel.cs
@@ -0,0 +1,18 @@
+﻿using PI.Utilities.Interfaces;
+using System;
+
+namespace PI.Utilities.Models
+{
+    /// <summary>
+    /// Basic implementation used by all models requiring an id
+    /// </summary>
+    public abstract class BaseIdWithQuickSearchModel : BaseIdModel, IAuditFields, IAuditFieldsWithId, IQuickSearchValue
+    {
+
+        /// <summary>
+        /// Gets/sets a quick search value for the object
+        /// </summary>
+        public virtual string QuickSearchValue { get; set; }
+
+    }
+}
---
+++ PI.Utilities/PI.Utilities/Models/BaseNameModel.cs
@@ -0,0 +1,34 @@
+﻿using PI.Utilities.Interfaces;
+using System;
+
+namespace PI.Utilities.Models
+{
+    /// <summary>
+    /// Basic implementation used by all models requiring a client id
+    /// </summary>
+    public abstract class BaseNameModel : BaseIdWithQuickSearchModel, IName, IAuditFields, IAuditFieldsWithId, IQuickSearchValue
+    {
+        private string _QuickSearchValue = "";
+        /// <summary>
+        /// Gets/sets a quick search value for the object
+        /// </summary>
+        public override string QuickSearchValue
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(_QuickSearchValue))
+                {
+                    _QuickSearchValue = Name;
+                }
+                return _QuickSearchValue;
+            }
+            set { _QuickSearchValue = value; }
+        }
+
+        /// <summary>
+        /// Gets/sets the item name
+        /// </summary>
+        public string Name { get; set; }
+
+    }
+}
